2ff30b53eb0d916d0040a3d4979999d93345bce1
GABRIEL ANTUNES <gabrielpedoa@gmail.com>
2024-07-18 17:00:40 -0300

    ajuste v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { useEffect } from "react";
 import MainContent from "./components/MainContent";
 import SideBar from "./components/SideBar";
 import "./styles/components/app.sass";
 
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+    });
+  }, []);
   return (
     <>
       <div id="portfolio" data-aos="fade-down" data-aos-delay="450">
